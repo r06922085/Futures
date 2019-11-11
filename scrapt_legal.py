40f2ca7d492bb23ce6c09d908e938503cdef30ab
Liocean <r06922085@csie.ntu.edu.tw>
2019-11-11 13:19:09 +0800

    Refactor

--- scrapt_legal.py
+++ scrapt_legal.py
@@ -1,11 +1,13 @@
-##抓取2008~的期貨開、收盤價
-import requests
-import re
+# 抓取2008~的期貨開、收盤價
 import datetime
-from datetime import date as date_time
-from library.library import *
-from bs4 import BeautifulSoup
+import re
 from array import array
+from datetime import date as date_time
+
+import requests
+from bs4 import BeautifulSoup
+
+from library.library import *
 
 
 class scrapt_legal():
@@ -15,69 +17,75 @@ class scrapt_legal():
         self.last_day = self.f[-1][0]
         self.get_data()
         print('The data has been update to the latest day!')
-    
-    def get_data(self): 
-        #set time information
-     
+
+    def get_data(self):
+        # set time information
+
         date = self.last_day
         today_hour = datetime.datetime.now().timetuple().tm_hour
-        
+
         if today_hour < 16:
-            today = self.date_format_transform(str(date_time.today()- datetime.timedelta(1)))
+            today = self.date_format_transform(
+                str(date_time.today() - datetime.timedelta(1)))
         else:
             today = self.date_format_transform(str(date_time.today()))
-            
+
         yy, mm, dd = self.get_date_detail(str(date))
-        
+
         while True:
             if date == today:
-                break;
-                
+                break
+
             payload_price = self.get_payload_price(dd, mm, yy, date)
-            
+
             reup = self.request(payload_price)
-            
+
             yy, mm, dd, date = self.renew_dates(yy, mm, dd)
-            
+
             if len(reup) > 0:
-                print(date) 
-               
+                print(date)
+
                 self.val = np.array((reup[9].text).split('\n'))
-                self.val = self.val[24].replace(',','')
+                self.val = self.val[24].replace(',', '')
                 self.val = int(self.val)
                 print(self.val)
                 #self.val = int(self.val[-1].replace(',',''))
                 self.write_txt(date)
-            
+
     def write_txt(self, date):
         print('writting...')
         self.fw.write(date)
         self.fw.write('  ')
         self.fw.write(str(self.val))
         self.fw.write('\n')
+
     def request(self, payload_price):
-        res = requests.post("http://www.taifex.com.tw/cht/3/futContractsDate",data = payload_price)
+        res = requests.post(
+            "http://www.taifex.com.tw/cht/3/futContractsDate", data=payload_price)
         res.encoding = 'utf8'
-        soup = BeautifulSoup(res.text,"html.parser")
+        soup = BeautifulSoup(res.text, "html.parser")
         reup = soup.select('.12bk')
         return reup
+
     def get_payload_price(self, dd, mm, yy, date):
         payload = {
-        'queryType': '3',
-        'goDay': '',
-        'doQuery': '1',
-        'dateaddcnt': '1',
-        'queryDate': date,
-        'commodityId': 'TXF'
+            'queryType': '3',
+            'goDay': '',
+            'doQuery': '1',
+            'dateaddcnt': '1',
+            'queryDate': date,
+            'commodityId': 'TXF'
         }
-        
+
         return payload
+
     def renew_dates(self, yy, mm, dd):
-        now = datetime.date(int(yy),int(mm),int(dd))
-        date = now + datetime.timedelta(days = 1)
+        now = datetime.date(int(yy), int(mm), int(dd))
+        date = now + datetime.timedelta(days=1)
         yy, mm, dd = self.get_date_detail(str(date))
-        date = yy+'/'+mm+'/'+dd    
-        return yy, mm, dd, date  
+        date = yy+'/'+mm+'/'+dd
+        return yy, mm, dd, date
+
     def get_date_detail(self, date):
         yy = ""
         mm = ""
@@ -85,17 +93,18 @@ class scrapt_legal():
         stage = 1
         for i in date:
             if ord(i) == 9 or ord(i) == ord('\n'):
-                break;
+                break
             elif i == '/' or i == '-':
-                stage = stage +1 
-            if i != '/' and i != '-':    
+                stage = stage + 1
+            if i != '/' and i != '-':
                 if stage == 1:
                     yy = yy + i
                 elif stage == 2:
                     mm = mm + i
                 elif stage == 3:
-                    dd = dd + i;    
+                    dd = dd + i
         return yy, mm, dd
+
     def date_format_transform(self, date):
         date_new = ''
         for i in date:
@@ -104,7 +113,7 @@ class scrapt_legal():
             else:
                 date_new += i
         return date_new
-        
+
+
 if __name__ == '__main__':
     s = scrap_legal()
-
--- scrapt_price.py
+++ scrapt_price.py
@@ -1,11 +1,14 @@
-##抓取2008~的期貨開、收盤價
-import requests
-import re
+# 抓取2008~的期貨開、收盤價
 import datetime
-from bs4 import BeautifulSoup
+import re
 from array import array
 from datetime import date as date_time
+
+import requests
+from bs4 import BeautifulSoup
+
 from library.library import *
+
 
 class scrapt_price(object):
     def __init__(self):
@@ -15,28 +18,30 @@ class scrapt_price(object):
         self.last_day = self.f[-1][0]
         self.get_price()
         print('The data has been update to the latest day!')
-    def get_price(self): 
+
+    def get_price(self):
         date = self.last_day
-        
+
         today_hour = datetime.datetime.now().timetuple().tm_hour
-        
+
         if today_hour < 16:
-            today = date_format_transform(str(date_time.today()- datetime.timedelta(1)))
+            today = date_format_transform(
+                str(date_time.today() - datetime.timedelta(1)))
         else:
             today = date_format_transform(str(date_time.today()))
-            
+
         yy, mm, dd = self.get_date_detail(str(date))
-        
+
         while True:
             if date == today:
-                break;
-            
+                break
+
             payload_price = self.get_payload_price(dd, mm, yy, date)
-            
+
             reup_price = self.request(payload_price)
-            
+
             yy, mm, dd, date = self.renew_dates(yy, mm, dd)
-            
+
             if len(reup_price) > 0:
                 print(date)
                 self.e_1 = str(int(reup_price[1].text))
@@ -46,9 +51,7 @@ class scrapt_price(object):
                 self.e_2_p = str(int(reup_price[17].text))
                 self.e_2_pp = str(int(reup_price[20].text))
                 self.write_txt(date)
-            
-            
-                
+
     def write_txt(self, date):
         print('writting...')
         self.fw.write(date)
@@ -65,11 +68,14 @@ class scrapt_price(object):
         self.fw.write('  ')
         self.fw.write(str(self.e_2_pp))
         self.fw.write('\n')
+
     def request(self, payload_price):
-        res_price = requests.post("http://www.taifex.com.tw/cht/3/futDailyMarketReport",data = payload_price)
-        soup_price = BeautifulSoup(res_price.text,"html.parser")
+        res_price = requests.post(
+            "http://www.taifex.com.tw/cht/3/futDailyMarketReport", data=payload_price)
+        soup_price = BeautifulSoup(res_price.text, "html.parser")
         reup_price = soup_price.select('.12bk')
         return reup_price
+
     def get_payload_price(sel, dd, mm, yy, date):
         payload_price = {
             'queryType': '3',
@@ -82,15 +88,17 @@ class scrapt_price(object):
             'commodity_idt': 'TX',
             'commodity_id2t': '',
             'commodity_id2t2': ''
-            }
-            
+        }
+
         return payload_price
+
     def renew_dates(self, yy, mm, dd):
-        now = datetime.date(int(yy),int(mm),int(dd))
-        date = now + datetime.timedelta(days = 1)
+        now = datetime.date(int(yy), int(mm), int(dd))
+        date = now + datetime.timedelta(days=1)
         yy, mm, dd = self.get_date_detail(str(date))
-        date = yy+'/'+mm+'/'+dd    
-        return yy, mm, dd, date  
+        date = yy+'/'+mm+'/'+dd
+        return yy, mm, dd, date
+
     def get_date_detail(self, date):
         yy = ""
         mm = ""
@@ -98,17 +106,18 @@ class scrapt_price(object):
         stage = 1
         for i in date:
             if ord(i) == 9 or ord(i) == ord('\n'):
-                break;
+                break
             elif i == '/' or i == '-':
-                stage = stage +1 
-            if i != '/' and i != '-':    
+                stage = stage + 1
+            if i != '/' and i != '-':
                 if stage == 1:
                     yy = yy + i
                 elif stage == 2:
                     mm = mm + i
                 elif stage == 3:
-                    dd = dd + i;    
+                    dd = dd + i
         return yy, mm, dd
-        
+
+
 if __name__ == '__main__':
     s = scrapt_price()
